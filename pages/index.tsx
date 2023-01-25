1bce750e6abfab0c365776b0448b623c11f09b56
Luis Dourado <lfaddourado@gmail.com>
2023-01-25 02:50:52 -0300

    feat: add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,6 +40,9 @@ function LinkCard({
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center mx-auto mt-16 px-8 ">
+      <Head>
+        <title>Links | {data.name}</title>
+      </Head>
       <Image
         alt={data.name}
         src={data.avatar}
