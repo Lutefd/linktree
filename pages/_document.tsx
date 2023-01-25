8249a61948e6a95550af8224e0453158f030b96a
Luis Dourado <lfaddourado@gmail.com>
2023-01-25 02:43:01 -0300

    feat: add bg gradiant

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-gradient-to-r from-blue-100 via-blue-300 to-blue-500">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
