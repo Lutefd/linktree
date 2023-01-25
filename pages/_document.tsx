6904094a0359215cdb5b56de3116391def3334ee
Luis Dourado <lfaddourado@gmail.com>
2023-01-25 02:50:46 -0300

    feat: change page language

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
       <body className="bg-gradient-to-r from-blue-100 via-blue-300 to-blue-500">
         <Main />
